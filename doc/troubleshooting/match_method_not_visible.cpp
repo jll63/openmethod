4c9fa925aac130f8ee50ced2844318aa3a466e94
Jean-Louis Leroy <jl@leroy.nyc>
2025-11-29 11:47:53 -0500

    troubleshooting guide

---
+++ doc/troubleshooting/match_method_not_visible.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2018-2025 Jean-Louis Leroy
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+// tag::content[]
+#include <boost/openmethod.hpp>
+
+namespace animals {
+
+struct Animal {
+    virtual ~Animal() {
+    }
+};
+
+struct Cat : Animal {};
+
+BOOST_OPENMETHOD(poke, (boost::openmethod::virtual_ptr<Animal>&), void);
+
+} // namespace animals
+
+BOOST_OPENMETHOD_OVERRIDE(
+    poke, (boost::openmethod::virtual_ptr<animals::Cat>&), void) {
+}
+// end::content[]
