9d414f8641ffbed67278571a529fed053bbea968
Jean-Louis Leroy <jl@leroy.nyc>
2026-03-14 14:03:30 -0400

    find dlls

--- test/dynamic_loading/main.cpp
+++ test/dynamic_loading/main.cpp
@@ -4,6 +4,7 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #define BOOST_TEST_MODULE dynamic_loading
+#define BOOST_TEST_INCLUDED
 #include <boost/test/unit_test.hpp>
 
 #define INCLUDED_FROM "main.cpp"
