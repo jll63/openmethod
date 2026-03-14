6edbb3c6ce2e2b42d110e0fc0a2df79e0e7800fe
Jean-Louis Leroy <jl@leroy.nyc>
2026-03-14 14:58:04 -0400

    prefix DLL file names

--- test/dynamic_loading/main.cpp
+++ test/dynamic_loading/main.cpp
@@ -66,11 +66,13 @@ BOOST_AUTO_TEST_CASE(test_shared_state) {
     constexpr auto global_mode =
         dll::load_mode::append_decorations /*| dll::load_mode::rtld_global*/;
 #endif
-    dll::shared_library registry_lib(lib_dir / "dl_test_registry", global_mode);
+    dll::shared_library registry_lib(
+        lib_dir / "boost_openmethod-dl_test_registry", global_mode);
     auto& registry_get_ids =
         registry_lib.get_alias<policy_ids_fn>("registry_get_ids");
 
-    dll::shared_library method_lib(lib_dir / "dl_test_method", global_mode);
+    dll::shared_library method_lib(
+        lib_dir / "boost_openmethod-dl_test_method", global_mode);
     auto& method_get_ids =
         method_lib.get_alias<policy_ids_fn>("method_get_ids");
 
@@ -83,7 +85,8 @@ BOOST_AUTO_TEST_CASE(test_shared_state) {
     BOOST_TEST(speak() == "?");
 
     dll::shared_library overrider_lib(
-        lib_dir / "dl_test_overrider", dll::load_mode::append_decorations);
+        lib_dir / "boost_openmethod-dl_test_overrider",
+        dll::load_mode::append_decorations);
     auto& overrider_get_ids =
         overrider_lib.get_alias<policy_ids_fn>("overrider_get_ids");
     auto& method_get_fn = method_lib.get_alias<method_fn>("method_get_fn");
@@ -94,57 +97,4 @@ BOOST_AUTO_TEST_CASE(test_shared_state) {
 
     registry_initialize();
     BOOST_TEST(speak() == "woof");
-
-#if 0
-    dll::shared_library overrider_lib(
-        lib_dir / "dl_test_overrider", dll::load_mode::append_decorations);
-
-    auto& method_get_registry_id =
-        registry_lib.get_alias<policy_ids_fn>("dl_registry_get_registry_id");
-    auto& method_get_policy_ids =
-        method_lib.get_alias<policy_ids_fn>("dl_method_get_policy_ids");
-    auto& method_get_method_fn =
-        method_lib.get_alias<method_fn_fn>("dl_method_get_method_fn");
-
-    auto& overrider_get_policy_ids =
-        overrider_lib.get_alias<policy_ids_fn>("dl_overrider_get_policy_ids");
-    auto& overrider_get_method_fn =
-        overrider_lib.get_alias<method_fn_fn>("dl_overrider_get_method_fn");
-
-    BOOST_TEST(registry_get_registry_id() == default_registry::id());
-
-    // Build local policy id array for comparison
-    const void* local_ids[n_policies + 1];
-    {
-        std::size_t i = 0;
-        mp11::mp_for_each<default_registry::policy_list>([&](auto p) {
-            using P = decltype(p);
-            if constexpr (detail::has_id<default_registry::policy<P>>) {
-                local_ids[i++] = default_registry::policy<P>::id();
-            }
-        });
-        local_ids[i] = nullptr;
-    }
-
-    const void** registry_ids = registry_get_policy_ids();
-    const void** method_ids = method_get_policy_ids();
-    const void** overrider_ids = overrider_get_policy_ids();
-
-    // All libraries must share the same policy static variables
-    for (std::size_t i = 0; local_ids[i] != nullptr; ++i) {
-        BOOST_TEST(registry_ids[i] == local_ids[i]);
-        BOOST_TEST(method_ids[i] == local_ids[i]);
-        BOOST_TEST(overrider_ids[i] == local_ids[i]);
-    }
-
-    // All libraries must see the same method object
-    BOOST_TEST(method_get_method_fn() == overrider_get_method_fn());
-
-#endif
-    // // Build dispatch tables (including the Dog overrider from the loaded lib)
-    // boost::openmethod::initialize();
-
-    // // Verify dispatch via the method
-    // Dog dog;
-    // BOOST_TEST(speak(dog) == "woof");
 }
