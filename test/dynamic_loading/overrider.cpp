7cc4700fcf5f737f255b7b29b222c2dee7fb1eb6
Jean-Louis Leroy <jl@leroy.nyc>
2026-03-29 15:07:00 -0400

    add guards

--- test/dynamic_loading/overrider.cpp
+++ test/dynamic_loading/overrider.cpp
@@ -3,7 +3,9 @@
 // See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#if defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic ignored "-Wunused-function"
+#endif
 
 #define INCLUDED_FROM "lib_overrider.cpp"
 
